7069c519015e8275730e5ea4c82bdffb7e3f7816
Joan Alba <picasoca@me.com>
2025-01-24 13:08:11 +0100

    sin advertencias

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,10 @@ import Home from '../views/Home.vue'
 import Inscripcion from '../views/Inscripcion.vue'
 import AsignacionMesas from '../views/mesas/AsignacionMesas.vue'
 import RegistroMesas from '../views/mesas/RegistroMesas.vue'
+import CrearCampeonato from '../views/CrearCampeonato.vue'
+import ModificarCampeonato from '../views/ModificarCampeonato.vue'
+import Ranking from '../views/resultados/Ranking.vue'
+import Podium from '../views/resultados/Podium.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,6 +15,16 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/crear-campeonato',
+      name: 'crear-campeonato',
+      component: CrearCampeonato
+    },
+    {
+      path: '/modificar-campeonato/:id',
+      name: 'modificar-campeonato',
+      component: ModificarCampeonato
     },
     {
       path: '/inscripcion/:campeonatoId',
@@ -26,6 +40,16 @@ const router = createRouter({
       path: '/mesas/registro/:campeonatoId',
       name: 'registro-mesas',
       component: RegistroMesas
+    },
+    {
+      path: '/resultados/ranking',
+      name: 'ranking',
+      component: Ranking
+    },
+    {
+      path: '/resultados/podium',
+      name: 'podium',
+      component: Podium
     }
   ]
 })
